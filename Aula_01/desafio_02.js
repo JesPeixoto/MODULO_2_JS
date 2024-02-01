44eeea75789f4d46f05f1497adac9e9613aad7af
Jessica Peixoto <jtpeixoto91@gmail.com>
2024-01-31 22:02:14 -0300

    Desafio 02

---
+++ Aula_01/desafio_02.js
@@ -0,0 +1,18 @@
+
+
+// Desafio 02 (Prompt https://jsfiddle.net/r1g6bhf7/)
+
+//let salario =prompt("Digite seu salário:")
+//let scoreDeCredito =prompt("Digite seu Score:")
+//let idade =prompt("Digite sua idade:")
+
+//alert( salario >= 5000 && scoreDeCredito >= 700 && idade >= 18)
+
+// Desafio 02 (Console.log)
+
+let salario = 10000
+let scoreDeCredito = 800
+let idade = 33
+
+console.log(salario >= 5000 && scoreDeCredito >= 700 && idade >= 18)
+
