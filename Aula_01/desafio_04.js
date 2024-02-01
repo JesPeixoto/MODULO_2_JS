d510312b5ebe0a7b969711a3591ea3cf2c72a5b5
Jessica Peixoto <jtpeixoto91@gmail.com>
2024-02-01 18:32:54 -0300

    Desafio 04

---
+++ Aula_01/desafio_04.js
@@ -0,0 +1,9 @@
+
+// Desafio 04 - Prompt 
+
+
+let idade = prompt("Digite a idade do cliente:")
+let compraMinima = prompt("Qual valor da compra?")
+let isNewClient = prompt("É cliente novo?")
+
+alert( idade >= 60 && compraMinima >= 100 && isNewClient === "false");
