1f7fac82373c92d2325cb73f5a0978bdb7c7706e
Jessica Peixoto <jtpeixoto91@gmail.com>
2024-01-31 21:43:23 -0300

    Desafio 01 - Utilizando prompt e console.log

---
+++ Aula_1/types.js
@@ -0,0 +1,14 @@
+
+
+// Desafio 01 (Com prompt)
+//let idade = prompt("Digite sua idade:")
+//let naciocalidade = prompt("Digite sua nacionalidade:")
+
+//alert(idade >= 16 && naciocalidade == "brasileira")
+
+
+// Desafio 01 (Com console.log)
+let idade = 10
+let naciocalidade = "brasileira"
+
+console.log(idade >= 16 && naciocalidade == "brasileira")
