0d9793dd81df058b9b02b6cb0e2105272ace84f9
Jessica Peixoto <jtpeixoto91@gmail.com>
2024-02-05 12:33:18 -0300

    Desafio 03

---
+++ Aula_02/Desafio_03.js
@@ -0,0 +1,19 @@
+
+
+// Desafio 03
+
+const matriz = [
+    [10, 5],
+    [3, 9]
+]
+
+const a = matriz[0] [0];
+const b = matriz[0] [1];
+const c = matriz[1] [0];
+const d = matriz[1] [1];
+
+const determinante = a * d - b * c
+
+console.log("Matriz é:");
+console.log(matriz);
+console.log("Determinante é:", determinante);
