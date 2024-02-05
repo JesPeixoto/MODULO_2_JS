2a094435692e3d0becb472d82539004c8568f82f
Jessica Peixoto <jtpeixoto91@gmail.com>
2024-02-05 20:07:35 -0300

    .

--- Aula_02/Desafio_05.js
+++ Aula_02/Desafio_05.js
@@ -3,4 +3,4 @@ const numeros = [3, 8, 15, 21, 30, 37, 42];
 
 const numerosPares = numeros.filter(numero => numero % 2 === 0);
 
-console.log('Os números pares são:', numerosPares);
+console.log('Os números pares são:', numerosPares); 
