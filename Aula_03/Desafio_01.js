e3cf5adb3952ba551d4b40319c27d796116f65e0
Jessica Peixoto <jtpeixoto91@gmail.com>
2024-02-06 16:12:34 -0300

    Desafio 01 - Feito por mim

---
+++ Aula_03/Desafio_01.js
@@ -0,0 +1,29 @@
+
+// Desafio 01
+
+ //Modifique a quantidade de um item específico da lista
+
+ const listaDeCompras = new Map();
+
+ listaDeCompras.set("Maca", "5");
+ listaDeCompras.set("Banana", "3");
+ listaDeCompras.set("Leite", "4");
+ listaDeCompras.set("Pao", "10");
+ 
+ //Verifique se um item específico está na lista de compras
+ console.log("Item específico: " + listaDeCompras.has("Pao"));
+
+ //Verifique a quantidade de um item específico da lista
+ console.log("O Quantidade do item na lista é : " + listaDeCompras.get("Leite"))
+
+ //Modifique a quantidade de um item específico da lista
+
+ console.log(listaDeCompras.set("Banana", "150"))
+ console.log("Quantidade atualizada: " + listaDeCompras.get("Banana"))
+
+
+ // Remova um item específico da lista
+ listaDeCompras.delete("Maca")
+ console.log(listaDeCompras)
+
+    
---
+++ Aula_03/tempCodeRunnerFile.js
@@ -0,0 +1,13 @@
+
+ const listaDeCompras = new Map();
+
+ listaDeCompras.set("Maca", "5");
+ listaDeCompras.set("Banana", "3");
+ listaDeCompras.set("Leite", "4");
+ listaDeCompras.set("Pao", "10");
+ 
+ // Retorna o tamanho do MAP - SIZE
+ console.log(listaDeCompras.size);
+ 
+
+ console.log(listaDeCompras.has("Pao"));
