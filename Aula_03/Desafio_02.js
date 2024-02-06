2c182f2913bd66b2ad8a8c21a4b93c143aa46aed
Jessica Peixoto <jtpeixoto91@gmail.com>
2024-02-06 16:30:56 -0300

    Desafio 02

---
+++ Aula_03/Desafio_02.js
@@ -0,0 +1,12 @@
+
+
+// Desafio 02
+// Crie uma agenda de contatos que armazene vários contatos em um array, 
+// modelando o "contato" como um objeto: (Nome, telefone, email) (Não utilizar Map)
+
+
+    const listaZerada = []
+
+    listaZerada.push = listaContatos = [ "Aline", "Palo", 27988031566, 278888888]
+
+    console.log(listaContatos)
