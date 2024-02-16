cbd584565ebcb51029db8a1a02591de433544d65
Jessica Peixoto <jtpeixoto91@gmail.com>
2024-02-16 13:27:12 -0300

    Desafio 03

--- Aula_05/Desafio_03.js
+++ Aula_05/Desafio_03.js
@@ -4,3 +4,24 @@
 //inteiros. Use um loop while para calcular a soma dos números
 //pares inseridos pelo usuário. Pare a entrada quando o usuário
 //inserir 0 e exiba a soma total.
+
+
+let somaPares = 0; 
+const numeros = [8, 10, 1, 0, 13]; 
+
+// Loop para calcular a soma dos números pares inseridos
+for (let i = 0; i < numeros.length; i++) {
+  const numeroInseridos = numeros[i];
+
+  // Verifica se o número inserido é 0
+  if (numeroInseridos === 0) {
+    break; // sendo ele finaliza
+  }
+
+  // Verifica se o número é par
+  if (numeroInseridos % 2 === 0) {
+    somaPares += numeroInseridos; // Se for par, adiciona o número à soma dos pares
+  }
+}
+
+console.log("A soma dos números pares inseridos é:", somaPares); // Exibe a soma total dos números pares inseridos
