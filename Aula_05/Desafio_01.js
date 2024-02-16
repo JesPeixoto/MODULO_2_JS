8a5c993d5b2fcd6d220fd0548ad6eae549f48fac
Jessica Peixoto <jtpeixoto91@gmail.com>
2024-02-16 13:16:27 -0300

    Desafio 01 e 02

--- Aula_05/Desafio_01.js
+++ Aula_05/Desafio_01.js
@@ -1,3 +1,9 @@
 
 // - Contagem Regressiva
 //● Escreva um programa que use um loop while para contar de 10 até 1 e imprima cada número na tela, em ordem decrescente.
+
+let contagem = 10; // inicio da contagem regressiva
+while (contagem >= 1) { // Inicia um loop while que continuará enquanto o valor da variável for maior ou igual a 1
+  console.log(contagem); 
+  contagem--; // Decrementa o valor da variável contagem em 1 a cada iteração do loop
+}
--- Aula_05/Desafio_02.js
+++ Aula_05/Desafio_02.js
@@ -4,3 +4,33 @@
 //use um loop while para verificar se o número é primo ou não. Um
 //número primo é aquele que só é divisível por 1 e por ele mesmo.
 //Imprima se o número é primo ou não.
+
+
+// Definindo o número para verificar se é primo
+let numero = 79; // Você pode alterar este número para qualquer outro que desejar
+
+// Se o número for menor ou igual a 1, não é primo
+if (numero <= 1) {
+    console.log(numero +  "não é um número primo.");
+} else {
+    let divisor = 2;
+    let primo = true;
+
+    // Enquanto o divisor for menor que o número, verificamos se há algum divisor
+    while (divisor < numero) {
+        // Se o número for divisível por qualquer outro número que não seja 1 ou ele mesmo, não é primo
+        if (numero % divisor === 0) {
+            primo = false;
+            break;
+        }
+        // Incrementamos o divisor para verificar os próximos números
+        divisor++;
+    }
+
+    // Se primo ainda for verdadeiro, o número é primo
+    if (primo) {
+        console.log(numero + " é um número primo.");
+    } else {
+        console.log(numero + " não é um número primo.");
+    }
+}
