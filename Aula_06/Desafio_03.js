edd6713658e9fd1ca25121675d7ae13c1375e9f2
Jessica Peixoto <jtpeixoto91@gmail.com>
2024-02-19 08:40:15 -0300

    .

---
+++ Aula_06/Desafio_03.js
@@ -0,0 +1,12 @@
+
+// Deafio 03
+
+/*Crie um array com pelo menos 5 nomes de frutas.
+Utilize um loop for-of para imprimir no console cada nome de fruta.*/
+
+const frutas = ["banana", "uva", "laranja", "kiwi", "amora"];
+
+for (const fruta of frutas) {
+    console.log(fruta)
+    
+}
