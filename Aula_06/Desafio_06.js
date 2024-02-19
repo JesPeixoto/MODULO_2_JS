72cbf6fbd2d406a33e5b5b20c09d2cbfabe42299
Jessica Peixoto <jtpeixoto91@gmail.com>
2024-02-19 09:19:04 -0300

    Desafio 06

--- Aula_06/Desafio_06.js
+++ Aula_06/Desafio_06.js
@@ -5,8 +5,8 @@
 [1,2,3,4,5,6,7,8,9,10]
 Achei o número 7 */
 
-const numeros = [1,2,3,4,5,6,7,8,9,10]; 
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; 
 
 const busca = numeros.find((numero) => numero === 5); 
 
-console.log(busca); 
+console.log(`Número ${busca} encontrado!`);
--- Aula_06/tempCodeRunnerFile.js
+++ Aula_06/tempCodeRunnerFile.js
@@ -1,5 +1,7 @@
+Achei o número 7 */
+
 const numeros = [1,2,3,4,5,6,7,8,9,10]; 
 
 const busca = numeros.find((numero) => numero === 5); 
 
-console.log(busca); 
+console.logi(´Número ${busca} encontrado); 
