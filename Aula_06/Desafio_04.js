51ae3e953499fb380f4461e8a6ff498ef4b65bff
Jessica Peixoto <jtpeixoto91@gmail.com>
2024-02-19 08:48:37 -0300

    Desafio 04

---
+++ Aula_06/Desafio_04.js
@@ -0,0 +1,14 @@
+
+// Desafio 04
+
+/* Escolha um número e imprima sua tabuada de 1 a 10 utilizando um loop
+for.
+Saída: N X 1 = 10 */
+
+
+const numero = 5; 
+
+for (let i = 1; i <= 10; i++) {
+    // Imprime a expressão da multiplicação usando interpolação de string
+    console.log(`${numero} x ${i} = ${numero * i}`);
+}
