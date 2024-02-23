fe45b743ce8aae40f67314a710d1628c709e12a1
Jessica Peixoto <jtpeixoto91@gmail.com>
2024-02-23 09:39:27 -0300

    Desafio 07

--- Aula_06/Desafio_07.js
+++ Aula_06/Desafio_07.js
@@ -1,1 +1,22 @@
 
+// Desafio 07
+
+// Ache a lógica: 1 1 2 3 5 8 13
+// Agora, sua tarefa é criar um programa que recebe um número inteiro N
+// como entrada e retorna os primeiros N termos da sequência.
+// Exemplo: Entrada: 6 Saída: [1 , 1, 2, 3, 5, 8]
+// Entrada: 14 Saída: [1, 1, 2, 3, 5, 8, 13, …, 377]
+
+function fibonacci(n) {
+    // começou com uma array sequencia contendo os dois primeiros números da sequência (1, 1)
+    let sequencia = [1, 1];
+  
+    for (let i = 2; i < n; i++) {
+      sequencia[i] = sequencia[i - 1] + sequencia[i - 2];
+    }
+  
+    return sequencia.slice(0, n);
+  }
+  
+  console.log(fibonacci(6));
+  console.log(fibonacci(14));
